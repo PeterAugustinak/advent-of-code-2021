383918166ba71cb8e94a06c2933a2829a2dd0771
p-a <peter.augustinak@gmail.com>
2021-12-02 09:30:52 +0100

    Consolidated solutions for day2

--- src/day3.py
+++ src/day3.py
@@ -3,6 +3,25 @@ with open("src/day2_input.txt", "r") as f:
 
 forward = 0
 deep = 0
+
+# part#1
+for instruction in data:
+    single_data = instruction.split()
+    if single_data[0] == 'up':
+        decrease = -int((single_data[1]))
+        deep += decrease
+    elif single_data[0] == 'down':
+        increase = int(single_data[1])
+        deep += increase
+    elif single_data[0] == 'forward':
+        increase = int(single_data[1])
+        forward += increase
+        
+print(forward * deep)
+
+# part#2
+forward = 0
+deep = 0
 aim = 0
 
 for instruction in data:
@@ -12,18 +31,12 @@ for instruction in data:
         aim += decrease
     elif single_data[0] == 'down':
         increase = int(single_data[1])
-        deep += increase
         aim += increase
     elif single_data[0] == 'forward':
         increase = int(single_data[1])
         forward += increase
         deep += aim * increase
-        
-print(forward)
-print(deep)
+
 print(forward * deep)
-
-
-
 
 
