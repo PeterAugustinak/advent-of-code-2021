29d6fb52083c69f157e150196354432c24e4b98e
p-a <peter.augustinak@gmail.com>
2021-12-06 17:12:03 +0100

    Day 6 - adjusted recursive function

--- src/day6.py
+++ src/day6.py
@@ -4,19 +4,14 @@ Full description: https://adventofcode.com/2021/day/6
 """
 
 # reading input data for the exercise
-from typing import final
-
-
 with open("src/inputs/day6_input.txt", "r") as f:
     data = [line.rstrip('\n') for line in f]
 
 lanterfish_start_lst = [int(fish) for fish in data[0].split(',')]
+
 # general functions
-
-day_left = 256
-
-def iterate_over_fish_lst(fish_lst):
-    global day_left
+def iterate_over_fish_lst(fish_lst, day_left):
+    """Recursive function to iterate throgh newly generated list"""
     new_fish_lst = []
     for fish in sorted(fish_lst):
         if fish == 0:
@@ -27,11 +22,14 @@ def iterate_over_fish_lst(fish_lst):
     day_left -= 1
     while day_left:
         print(day_left, len(new_fish_lst))
-        return iterate_over_fish_lst(new_fish_lst)
+        return iterate_over_fish_lst(new_fish_lst, day_left)
     else:
         return new_fish_lst
 
-final_lanterfish_lst = iterate_over_fish_lst(lanterfish_start_lst)
+final_lanterfish_lst = iterate_over_fish_lst(lanterfish_start_lst, 80)
 print(len(final_lanterfish_lst))
+
+
+
 
 
