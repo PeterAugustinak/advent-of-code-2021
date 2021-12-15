80da8cd6928c201486f1e303532bb928d3960f81
p-a <peter.augustinak@gmail.com>
2021-12-15 15:49:26 +0100

    Day 15 - part I. - adjusted code to handle various track in case of same number, but incorrect solution

--- src/day15.py
+++ src/day15.py
@@ -1,5 +1,5 @@
 """
-ADVENT OF CODE DAY 
+ADVENT OF CODE DAY 15 - CHITON
 Full description: https://adventofcode.com/2021/day/15
 """
 
@@ -14,7 +14,7 @@ def part1(data):
     """Solve part 1"""
     labyrint = [row for row in data]
     total_risk = navigate_via_labyrint(labyrint)
-    return total_risk
+    return min(path_lst)
 
 
 def part2(data):
@@ -28,28 +28,34 @@ def solve(day):
 
     return solution1, solution2
 
+path_lst = []
+
 # SOLUTION FUNCTIONS
-def navigate_via_labyrint(labyrint, current_position=(0, 0), total_risk=0, current_direction='right'):
+def navigate_via_labyrint(labyrint, current_position=(0, 0), total_risk=0, previous_risk=0):
     """
     Navigating through labyrint by recursively calling determine_next_step_in_labyrint method
     and counting the total risk number.
     """
     while current_position:
-        next_position, total_risk, current_direction = determine_next_step_in_labyrint(labyrint, current_position, current_direction, total_risk)
-        return navigate_via_labyrint(labyrint, next_position, total_risk, current_direction)
-    else:
-        return total_risk
+        try:
+            labyrint, next_position, total_risk, previous_risk = determine_next_step_in_labyrint(labyrint, current_position, total_risk, previous_risk)
+            return navigate_via_labyrint(labyrint, next_position, total_risk, previous_risk)
+        except ValueError:
+            break
+        except TypeError:
+            break
 
 
-def determine_next_step_in_labyrint(labyrint,  current_position, current_direction, total_risk):
+
+def determine_next_step_in_labyrint(labyrint, current_position, total_risk, previous_risk):
     """
     Determines what is the best next step with follwing rules:
     - only can go to the right or down - means i+1 or row[1][i]
     - if the same numbers are left and row, favor the current moving direction
     """
-    current_direction = 'right'
-    r, c = current_position
 
+    risk = None
+    r, c = current_position
     right_position = (r, c+1)
     down_position = (r+1, c)
 
@@ -63,36 +69,43 @@ def determine_next_step_in_labyrint(labyrint,  current_position, current_directi
         down_risk = None
 
     if right_risk and down_risk:
-        next_position, risk, current_direction = evaluate_next_step_if_both_exist(right_risk, down_risk, right_position, down_position, current_direction)
+        if right_risk == down_risk:
+            new_labyrint = cut_labyrint(labyrint, r, c)
+            return navigate_via_labyrint(new_labyrint, right_position, total_risk, previous_risk), navigate_via_labyrint(new_labyrint, down_position, total_risk, previous_risk)
+        else:
+            next_position, risk = evaluate_next_step_if_both_exist(right_risk, down_risk, right_position, down_position)
+    elif not right_risk and not down_risk:
+        global path_lst
+        if previous_risk == 7:
+            path_lst.append(total_risk)
     else:
-        next_position, risk, current_direction = evaluate_next_step_when_border(right_risk, down_risk, right_position, down_position, )
+        next_position, risk = evaluate_next_step_when_border(right_risk, down_risk, right_position, down_position)
 
-    print(risk)
-    total_risk += risk
-    return next_position, total_risk, current_direction
+    
+    if risk:
+        total_risk += risk
+        return labyrint, next_position, total_risk, risk
 
-def evaluate_next_step_if_both_exist(right_risk, down_risk, right_position, down_position, current_direction):
+def cut_labyrint(labyrint, r, c):
+    """Cuts current labyrint to only remanining part."""
+    new_labyrint = [row[c:] for row in labyrint[r:]]
+    return new_labyrint
+
+def evaluate_next_step_if_both_exist(right_risk, down_risk, right_position, down_position):
     """If both right and left step exists, it determines what next step is better."""
 
     if right_risk < down_risk:
-        return right_position, right_risk, 'right'     
+        return right_position, right_risk    
     elif down_risk < right_risk:
-        return down_position, down_risk, 'down'
-    elif right_risk == down_risk:
-        if current_direction == 'right':
-            return right_position, right_risk, 'right'
-        elif current_direction == 'down':
-            return down_position, down_risk, 'down'
+        return down_position, down_risk
 
 def evaluate_next_step_when_border(right_risk, down_risk, right_position, down_position):
     """If there is most right of most down position, or the final step (right down), then it evaluates specifically."""
     if right_risk:
-        return right_position, right_risk, 'right'     
+        return right_position, right_risk    
     elif down_risk:
-        return down_position, down_risk, 'down'
-    else:
-        return None, 0, None
-    
+        return down_position, down_risk
+
 
 
 
